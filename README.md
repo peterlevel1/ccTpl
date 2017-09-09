255c0c3da213cff6105b0b57588cfa4b1a46b8e4
peter <1785103669@qq.com>
2017-09-10 02:17:41 +0800

    all commits

--- README.md
+++ README.md
@@ -1,8 +1,73 @@
-# ccTpl
+# cc-template
 
-# The fastest javascript template in the world !
-# The simplest javascript template in the world !
+### a nice template which deserve your notice !
+### it is an Embedded JavaScript templates.
 
-## This is ccTpl !
-## You are using a wonderful template which I think I got the secrets of the template, and I would like to share this with jsers !
+## Features
 
+  * custom define the scope name `options.scope = 'theNameILike'`
+  * custom define the regexpHead and the regexpTail `options.regexpHead = '{{'`
+  * Unbuffered code for conditionals etc `<% code %>`
+  * Escapes html by default with `<%= code %>`
+  * Unescaped buffering with `<%- code %>`
+  * dangerously insert data with `<%! code %>`
+
+## Example
+
+    <% if (scope.user) { %>
+	    <h2><%= scope.user.name %></h2>
+    <% } %>
+
+## Try out a live example now
+
+## Usage
+
+    var compiled = new cc.Template().compile(str);
+    // => Function
+
+    compiled(data);
+    // => str
+
+## Options
+  - `scope`       The scope name within the template string, defaulting to "scope"
+  - `regexpHead`  Open tag, defaulting to "<%"
+  - `regexpTail`  Closing tag, defaulting to "%>"
+
+## Custom delimiters
+
+Custom delimiters can also be applied globally:
+
+    var template = new cc.Template({
+      scope: 'it',
+      regexpHead: '{{',
+      regexpTail: '}}'
+    });
+
+Which would make the following a valid template:
+
+    <h1>{{= it.title }}</h1>
+
+## License
+
+(The MIT License)
+
+Copyright (c) 2017-2099 CongCong
+
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+'Software'), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
---
+++ dist/cc-template.js
@@ -0,0 +1,169 @@
+;(function (global, factory) {
+  if (typeof module !== 'undefined' && module && module.exports) {
+    module.exports = factory();
+  } else if (typeof define === 'function' && define.amd) {
+    define(function () {
+      return factory();
+    });
+  } else if (typeof define === 'function' && define.cmd) {
+    define(function (require, exports, module) {
+      module.exports = factory();
+    });
+  } else {
+    global.cc = global.cc || {};
+    global.cc.Template = factory();
+  }
+})(this, function() {
+
+  // List of HTML entities for escaping.
+  var escapeMap = {
+    '&': '&amp;',
+    '<': '&lt;',
+    '>': '&gt;',
+    '"': '&quot;',
+    "'": '&#x27;',
+    '`': '&#x60;'
+  };
+
+  var unescapeMap = invert(escapeMap);
+
+  function invert(obj) {
+    return Object.keys(obj)
+      .reduce(function (memo, key, index) {
+        var val = obj[key];
+        memo[val] = key;
+        return memo;
+      }, {});
+  }
+
+  function defaults(a, b) {
+    for (var p in a) {
+      if (a.hasOwnProperty(p) && b[p] !== void 0) {
+        a[p] = b[p];
+      }
+    }
+
+    return a;
+  }
+
+  function Template(options) {
+    options = options || {};
+
+    if (options.escapeMap) {
+      this.escape = this.createEscaper(options.escapeMap);
+    }
+
+    if (options.unescapeMap) {
+      this.unescape = this.createEscaper(options.unescapeMap);
+    }
+
+    this.options = defaults(
+      Template.getDefaultOptions(),
+      options
+    );
+  }
+
+  Template.getDefaultOptions = function () {
+    return {
+      scope: 'scope',
+      regexpHead: '<%',
+      regexpTail: '%>',
+    };
+  };
+
+  // Functions for escaping and unescaping strings to/from HTML interpolation.
+  Template.createEscaper = function(map) {
+    var escaper = function(match) {
+      return map[match];
+    };
+    // Regexes for identifying a key that needs to be escaped.
+    var source = '(?:' + Object.keys(map).join('|') + ')';
+    var testRegexp = RegExp(source);
+    var replaceRegexp = RegExp(source, 'g');
+    return function(string) {
+      string = string == null ? '' : '' + string;
+      return testRegexp.test(string) ? string.replace(replaceRegexp, escaper) : string;
+    };
+  };
+
+  Template.prototype.escape = Template.createEscaper(escapeMap);
+
+  Template.prototype.unescape = Template.createEscaper(unescapeMap);
+
+  Template.prototype.createEscaper = Template.createEscaper;
+
+  Template.prototype._getTemplateRegExp = function () {
+    return new RegExp(this.options.regexpHead + '([\\s\\S]+?)' + this.options.regexpTail, 'g');
+  };
+
+  Template.prototype.compile = function (str) {
+    str = str.trim();
+    var reg = this._getTemplateRegExp();
+    var match;
+    var sign;
+    var content;
+    var ret = 'var _ret = "";\n';
+    var lastIndex = 0;
+
+    while (match = reg.exec(str)) {
+      sign = match[1][0];
+      content = match[1].slice(1).trim();
+
+      ret += '_ret += "' + str.slice(lastIndex, match.index).replace(/\n/g, '\\n') + '";\n';
+
+      switch (sign) {
+        case '=':
+          ret += '_ret += this.escape(' + content + ');\n';
+          break;
+        case '-':
+          ret += '_ret += this.unescape(' + content + ');\n';
+          break;
+        case '!':
+          ret += '_ret += ((' + content + ' || "") + "")' + ';\n';
+          break;
+        // TODO sub templates nested rendering
+        case '@':
+          break;
+        default:
+          ret += content + '\n';
+          break;
+      }
+
+      lastIndex = match.index + match[0].length;
+    }
+
+    ret += '_ret += "' + str.slice(lastIndex).trim().replace(/\n/g, '') + '";\n';
+
+    ret += 'return _ret;\n';
+
+    var render;
+    try {
+      render = new Function(this.options.scope, ret);
+    } catch (error) {
+      error.source = ret;
+      throw error;
+    }
+
+    var self = this;
+
+    var compiled = function (scope, ctx) {
+      if (ctx) {
+        if (!ctx.escape) {
+          ctx.escape = self.escape;
+        }
+
+        if (!ctx.unescape) {
+          ctx.escape = self.unescape;
+        }
+      }
+
+      return render.call(ctx || self, scope);
+    };
+
+    compiled.source = 'function(' + this.options.scope + '){\n' + ret + '}';
+
+    return compiled;
+  };
+
+  return Template;
+});
